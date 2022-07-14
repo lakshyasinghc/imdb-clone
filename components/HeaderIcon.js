3f7b412fbe3481cefcefae8d23a9ca0c131f5447
lakshya singh chouhan <lakshyasinghchouhan2509@gmail.com>
2022-07-14 11:44:11 -0400

    style the header section

--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function HeaderIcon({Icon,title}) {
   return (
-    <div>
-        <Icon className="h-8"/>
-        <p>{title}</p>
+    <div className="mx-4 flex flex-col  cursor-pointer hover:text-white lg:mx-6 active:text-red-400">
+        <Icon className="h-8 "/>
+        <p className='my-2'> {title}</p>
     </div>
   )
 }
