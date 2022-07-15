4ddf300af57cf0d1e385824f50eca6e620ac372f
lakshya singh chouhan <lakshyasinghchouhan2509@gmail.com>
2022-07-15 12:15:13 -0400

    create card component

---
+++ components/Card.js
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import React from 'react'
+import { ThumbUpIcon } from '@heroicons/react/outline';
+
+export default function Card({result}) {
+    const BASE_URL="https://image.tmdb.org/t/p/original";
+  return (
+    <div>
+        <Image layout='responsive' src={BASE_URL +result.backdrop_path} width={100} height={100}/>
+        <div >
+            <p className="truncate"> {result.overview}</p>
+            <h2>{result.title || result.name}</h2>
+            <p>{result.release_date||result.first_air_date}
+            <ThumbUpIcon className='h-5'/>
+            {result.vote_count}
+            </p>
+           
+        </div>
+    </div>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   swcMinify: true,
   images:
   {
-    domains:["upload.wikimedia.org"],
+    domains:["upload.wikimedia.org","image.tmdb.org"],
   }
 }
 
