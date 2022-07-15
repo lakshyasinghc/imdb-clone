1e9b6741eb54cc23d4144c3eb18ae414763ce046
lakshya singh chouhan <lakshyasinghchouhan2509@gmail.com>
2022-07-15 11:10:44 -0400

    create server side function and pass it to client side

--- components/Results.js
+++ components/Results.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function Results() {
+export default function Results({results}) {
   return (
     <div>
-        <h1>
-            Results is here
-        </h1>
+      {results.map(result=>(
+        <h1>{result.title}</h1>
+      ))}
     </div>
   )
 }
--- utils/requests.js
+++ utils/requests.js
@@ -3,10 +3,10 @@ const API_KEY=process.env.API_KEY;
 export default {
     fetchTrending:{
         title:"Trending",
-        url:`/trending/all/week?api_key=${API_KEY}language=en-US`
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
     },
     fetchTopRated:{
         title:"Top Rated",
-        url:`/movie/top_rated?api_key=${API_KEY}language=en-US`
+        url:`/movie/top_rated?api_key=${API_KEY}&language=en-US`
     }
 }
